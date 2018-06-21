04467a38b07b2b66e34f8fd2ad86cd3851245af0
EleMint <dylanschmidt0216@gmail.com>
2018-06-21 10:40:30 -0500

    Addition of multi-player logic

--- RPSLS/ScoreBoardMulti.cs
+++ RPSLS/ScoreBoardMulti.cs
@@ -11,6 +11,7 @@ namespace RPSLS
         //Member Variables (HAS A)
         public int playerOneScore;
         public int playerTwoScore;
+        public string roundWinner;
 
         //Constructor
         public ScoreBoardMulti()
@@ -20,18 +21,160 @@ namespace RPSLS
         }
 
         //Member Methods (CAN DO)
-        public void DisplayScore(string playerOne)
+
+        public void DisplayScore(string playerOne, string playerTwo)
         {
-            Console.WriteLine(playerOne + ": " + this.playerOneScore + "\r\n" + "Player Two Score: " + playerTwoScore + "\r\n");
+            Console.WriteLine("\r\n" + "--Scores--" + "\r\n" + playerOne + ": " + this.playerOneScore + "\r\n" + "Computer: " + playerTwoScore);
+            if (playerOneScore == 2)
+            {
+                this.DisplayWinner("\r\n" + playerOne + "Is The Winner!");
+            }
+            else if (playerTwoScore == 2)
+            {
+                this.DisplayWinner("\r\n" + playerTwo + "Is The Winner!");
+            }
         }
-        public void AddPlayerOnePoint()
+
+        public void DecideRound(string playerOneMove, string playerTwoMove)
+        {
+            switch (playerOneMove)
+            {
+                case "rock":
+                    switch (playerTwoMove)
+                    {
+                        case "rock":
+                            roundWinner = null;
+                            break;
+                        case "paper":
+                            roundWinner = "playerTwo";
+                            break;
+                        case "scissors":
+                            roundWinner = "playerOne";
+                            break;
+                        case "lizard":
+                            roundWinner = "playerOne";
+                            break;
+                        case "spock":
+                            roundWinner = "playerTwo";
+                            break;
+                    }
+                    break;
+                case "paper":
+                    switch (playerTwoMove)
+                    {
+                        case "rock":
+                            roundWinner = "playerOne";
+                            break;
+                        case "paper":
+                            roundWinner = null;
+                            break;
+                        case "scissors":
+                            roundWinner = "playerTwo";
+                            break;
+                        case "lizard":
+                            roundWinner = "playerTwo";
+                            break;
+                        case "spock":
+                            roundWinner = "playerOne";
+                            break;
+                    }
+                    break;
+                case "scissors":
+                    switch (playerTwoMove)
+                    {
+                        case "rock":
+                            roundWinner = "playerTwo";
+                            break;
+                        case "paper":
+                            roundWinner = "playerOne";
+                            break;
+                        case "scissors":
+                            roundWinner = null;
+                            break;
+                        case "lizard":
+                            roundWinner = "playerOne";
+                            break;
+                        case "spock":
+                            roundWinner = "playerTwo";
+                            break;
+                    }
+                    break;
+                case "lizard":
+                    switch (playerTwoMove)
+                    {
+                        case "rock":
+                            roundWinner = "playerTwo";
+                            break;
+                        case "paper":
+                            roundWinner = "playerOne";
+                            break;
+                        case "scissors":
+                            roundWinner = "playerTwo";
+                            break;
+                        case "lizard":
+                            roundWinner = null;
+                            break;
+                        case "spock":
+                            roundWinner = "playerOne";
+                            break;
+                    }
+                    break;
+                case "spock":
+                    switch (playerTwoMove)
+                    {
+                        case "rock":
+                            roundWinner = "playerOne";
+                            break;
+                        case "paper":
+                            roundWinner = "playerTwo";
+                            break;
+                        case "scissors":
+                            roundWinner = "playerOne";
+                            break;
+                        case "lizard":
+                            roundWinner = "playerTwo";
+                            break;
+                        case "spock":
+                            roundWinner = null;
+                            break;
+                    }
+                    break;
+            }
+
+            if (roundWinner == "playerOne")
+            {
+                this.AddPointPlayerOne();
+            }
+            else if (roundWinner == "playerTwo")
+            {
+                this.AddPointPlayerTwo();
+            }
+            else
+            {
+                Console.WriteLine("\r\n" + "The Round Ended In A Tie");
+            }
+        }
+
+        public void AddPointPlayerOne()
         {
             playerOneScore += 1;
         }
 
-        public void AddPlayerTwoPoint()
+        public void AddPointPlayerTwo()
         {
             playerTwoScore += 1;
+        }
+
+        public void DisplayWinner(string winner)
+        {
+            if (winner == "playerOne")
+            {
+                Console.WriteLine("\r\n" + "Congratulations, " + PlayerOne + ", You've Beat The Computer!");
+            }
+            else
+            {
+                Console.WriteLine("\r\n" + "Congratulations, " + PlayerTwo + ", You've Beat The Computer!");
+            }
         }
     }
 }
--- RPSLS/TwoPlayerGame.cs
+++ RPSLS/TwoPlayerGame.cs
@@ -9,9 +9,34 @@ namespace RPSLS
     class TwoPlayerGame
     {
         //Member Variables (HAS A)
-
+        public string playerOneName;
+        public string playerTwoName;
+        public string playerMove;
+        public string computerMove;
         //Constructor
-
-        //Member Methods (CAN DO)
+        public TwoPlayerGame()
+        {
+            Console.WriteLine("\r\n" + "This is A Two Player Game.");
+            Console.WriteLine("\r\n" + "Player One, Please Enter Your Name:");
+            playerOneName = Console.ReadLine();
+            Console.WriteLine("\r\n" + "Player Two, Please Enter Your Name:");
+            playerTwoName = Console.ReadLine();
+            PlayerOne playerOne = new PlayerOne(playerOneName);
+            PlayerTwo playerTwo = new PlayerTwo(playerTwoName);
+            ScoreBoardMulti scoreBoard = new ScoreBoardMulti();
+            Console.WriteLine("\r\n" + "Hello, " + playerOne.name + "!");
+            scoreBoard.DisplayScore(playerOne.name);
+            Console.WriteLine("\r\n" + "The Scores Are Set To 0. Let's Begin!");
+            do
+            {
+                playerMove = playerOne.MakeMove();
+                computerMove = computer.MakeMove();
+                computer.ShowMove(computerMove);
+                scoreBoard.DecideRound(playerMove, computerMove);
+                scoreBoard.DisplayScore(playerOne.name);
+                Console.ReadLine();
+            }
+            while (scoreBoard.playerScore < 2 && scoreBoard.computerScore < 2);
+        }
     }
 }
